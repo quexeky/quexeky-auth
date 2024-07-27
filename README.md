a58c087f45b2367d40762a26bcdad1aa37a66106
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-07-27 22:30:06 +1000

    Converted to using 16 characters isntead of 64 because 64 was wayyy too many

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # quexeky-auth
-An authentication service for my own websites. Learning how credential management systems, OAuth, and other things work. Hosted on CloudFlare
+An authentication service for my own websites. Learning how credential management systems,
+OAuth, and other things work. Hosted on CloudFlare
+
+# Specifications
+All identifiers are to be done through 16 character base64 strings (256 bits)
--- src/endpoints/requestUser.ts
+++ src/endpoints/requestUser.ts
@@ -5,8 +5,8 @@ export class RequestUser extends OpenAPIRoute {
     schema = {
         request: {
             query: z.object({
-                user_id: z.string().base64().length(64),
-                application_id: z.string().base64().length(64),
+                user_id: z.string().base64().length(16),
+                application_id: z.string().base64().length(16),
             })
         }
     }
@@ -18,6 +18,6 @@ export class RequestUser extends OpenAPIRoute {
         const result = await c.env.DB.prepare(
             "SELECT * FROM users WHERE user_id = ?1 AND application_id = ?2",
         ).bind(data.query.user_id, data.query.application_id).run();
-        return result.results.permissions;
+        return result.results;
     }
 }
