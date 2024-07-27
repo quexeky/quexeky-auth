116e29776727d90684d3343760ca9be000240a32
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-07-27 22:18:26 +1000

    Removed old code and removed additional metadata from user requests

--- src/endpoints/requestUser.ts
+++ src/endpoints/requestUser.ts
@@ -1,7 +1,7 @@
 import { OpenAPIRoute } from "chanfana";
 import { z } from "zod";
 
-export class GetToken extends OpenAPIRoute {
+export class RequestUser extends OpenAPIRoute {
     schema = {
         request: {
             query: z.object({
@@ -15,8 +15,9 @@ export class GetToken extends OpenAPIRoute {
 
         console.log(data);
 
-        return await c.env.DB.prepare(
+        const result = await c.env.DB.prepare(
             "SELECT * FROM users WHERE user_id = ?1 AND application_id = ?2",
         ).bind(data.query.user_id, data.query.application_id).run();
+        return result.results.permissions;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
-import { GetToken } from "./endpoints/getToken";
+import { RequestUser } from "./endpoints/requestUser";
 
 // Start a Hono app
 const app = new Hono();
@@ -14,8 +10,8 @@ const openapi = fromHono(app, {
 	docs_url: "/",
 });
 
-// Register OpenAPI endpoints
-openapi.get("/token", GetToken);
+// Checking user data
+openapi.get("/user", RequestUser);
 
 // Export the Hono app
 export default app;
