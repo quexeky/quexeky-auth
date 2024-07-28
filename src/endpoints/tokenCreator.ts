a6d31a86acceaaab3c9c3fd2f95960cd6e00d314
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-07-29 09:59:56 +1000

    Update on tokenCreator.ts

---
+++ src/endpoints/tokenCreator.ts
@@ -0,0 +1,36 @@
+import { OpenAPIRoute } from "chanfana";
+import { z } from "zod";
+import {compareSync, hashSync} from "bcryptjs";
+
+export class TokenCreator extends OpenAPIRoute {
+    schema = {
+        request: {
+            query: z.object({
+                username: z.string().max(32),
+                password: z.string().base64().length(8), // 512 bit password hash
+                application_id: z.string().base64().length(16)
+            })
+        }
+    }
+    async handle(c) {
+        const data = await this.getValidatedData<typeof this.schema>();
+
+        openapi.
+
+        const recvPassword = data.query.password;
+
+        const password = hashSync(recvPassword);
+        console.log("Password:", password);
+
+        console.log("User:", data.query.username);
+
+        const result = await c.env.DB.prepare(
+            "INSERT INTO users(username, password) VALUES(?1, ?2)"
+        ).bind(data.query.username, password).run();
+
+        console.log(result);
+
+        return result;
+    }
+}
+
