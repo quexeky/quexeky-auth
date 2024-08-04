44a26abe27d6ea11bbc23e47aa8e791354da6a7e
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-08-04 10:29:14 +1000

    Progress with JSON web tokens

--- src/endpoints/tokenCreator.ts
+++ src/endpoints/tokenCreator.ts
@@ -1,6 +1,7 @@
 import { OpenAPIRoute } from "chanfana";
 import { z } from "zod";
 import {worker_fetch} from "../util";
+import jwt from "@tsndr/cloudflare-worker-jwt";
 
 export class TokenCreator extends OpenAPIRoute {
     schema = {
@@ -8,12 +9,15 @@ export class TokenCreator extends OpenAPIRoute {
             query: z.object({
                 username: z.string().max(32),
                 password: z.string().base64().length(8), // 512 bit password hash
-                application_id: z.string().base64().length(16)
+                application_id: z.string().base64().length(16),
+                permissions: z.array(z.string())
             })
         }
     }
     async handle(c) {
         const data = await this.getValidatedData<typeof this.schema>();
+
+        const { username, password, application_id } = data.query;
 
         const user_login = await worker_fetch("api/userLogin", JSON.stringify(
             { username: data.query.username, password: data.query.password }
@@ -23,12 +27,22 @@ export class TokenCreator extends OpenAPIRoute {
             return new Response(undefined, {status: user_login.status});
         }
 
-        const application_id = data.query.application_id;
 
-        const token = new Uint8Array(64);
+        /*const token = new Uint8Array(64);
         crypto.getRandomValues(token);
 
         const encoded_token = btoa(String.fromCharCode(...token));
+         */
+
+        const token = await jwt.sign(
+            {
+                username: data.query.username,
+                application_id: application_id,
+                permissions: data.query.permissions,
+            },
+            c.env.JWT_SIGNATURE,
+            { algorithm: "ES256" }
+        )
 
         console.log(application_id);
 
@@ -36,7 +50,7 @@ export class TokenCreator extends OpenAPIRoute {
 
         const result = await c.env.DB.prepare(
             "INSERT INTO tokens(username, application_id, token) VALUES(?, ?, ?)"
-        ).bind(data.query.username, application_id, encoded_token).run();
+        ).bind(data.query.username, application_id, token).run();
 
 
         return new Response(JSON.stringify({
